3224cf0ec219c9b1195272a6f7c491e732dabc44
Valentin ROQUEJOFRE <valentin.roquejofre@epitech.eu>
2025-02-08 10:03:10 +0100

    fix enemy and add menu

--- don't-get-surrounded/enemy/enemy.gd
+++ don't-get-surrounded/enemy/enemy.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 var speed = 30
 var life = 10
 
+var death_animation_repetition = 2
+var death_time = 1
+
 @export var base: Node2D
 @onready var nav_agent:= $NavigationAgent2D as NavigationAgent2D
 
@@ -11,35 +14,41 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_released("ui_left"):
+		life = 0
+	if (life <= 0 and death_time >= 0):
+		death_time -= 1
 		death()
 
 func _physics_process(delta: float) -> void:
 	var dir = to_local(nav_agent.get_next_path_position()).normalized()
 	velocity = dir * speed
 	move_and_slide()
-	
+
 func makepath() ->void:
 	nav_agent.target_position = base.global_position
 
 func _on_timer_timeout() -> void:
-	if (velocity.x <= 0):
-		$Sprite2D.flip_h = true
-	if (velocity.x > 0):
-		$Sprite2D.flip_h = false
 	if (life > 0):
-		print($Sprite2D.frame)
-		if ($Sprite2D.frame >= 5):
-			$Sprite2D.frame = 0
-		$Sprite2D.frame += 1
-	makepath()
+		if (velocity.x <= 0):
+			$Sprite2D.flip_h = true
+		if (velocity.x > 0):
+			$Sprite2D.flip_h = false
+		if (life > 0):
+			print($Sprite2D.frame)
+			if ($Sprite2D.frame >= 5):
+				$Sprite2D.frame = 0
+			$Sprite2D.frame += 1
+		makepath()
 
 func death() -> void:
 	$TimerDeath.start()
-	$Sprite2D.hide()
-	$Death.show()
-	if ($Death.frame == 3):
-		queue_free()
-	pass
+	$Sprite2D.visible = false
+	$Death.visible = true
 
 func _on_timer_death_timeout() -> void:
+	if ($Death.frame == 3):
+		if (death_animation_repetition <= 0):
+			queue_free()
+		death_animation_repetition -= 1
+		$Death.frame = 0
 	$Death.frame += 1
