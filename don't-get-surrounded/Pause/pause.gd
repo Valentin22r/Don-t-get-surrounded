6c6971d14ece8bf04f8b85fa51b9cf5d2e39a23a
Valentin ROQUEJOFRE <valentin.roquejofre@epitech.eu>
2025-02-09 18:46:20 +0100

    a

--- don't-get-surrounded/Pause/pause.gd
+++ don't-get-surrounded/Pause/pause.gd
@@ -1,7 +1,13 @@
 extends Control
 
 
+func _on_continue_button_pressed() -> void:
+	hide()
+	Engine.time_scale = 1
+
 func _on_back_button_pressed() -> void:
+	Engine.time_scale = 1
 	$click_buttons.play()
 	await get_tree().create_timer(0.25).timeout
+	hide()
 	get_tree().change_scene_to_file("res://Menu/menu.tscn")
