133fe0addf5f41960da180ee6ebfaa9ebec68f6a
Valentin ROQUEJOFRE <valentin.roquejofre@epitech.eu>
2025-02-09 17:33:09 +0100

    de

---
+++ don't-get-surrounded/Intro/Intro.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _on_back_button_pressed() -> void:
+	$click_buttons.play()
+	await get_tree().create_timer(0.25).timeout
+	get_tree().change_scene_to_file("res://Map/map.tscn")
+
+
+func _on_continue_button_pressed() -> void:
+	hide()
+	Engine.time_scale = 1
--- don't-get-surrounded/Menu/menu.gd
+++ don't-get-surrounded/Menu/menu.gd
@@ -4,8 +4,7 @@ extends Control
 func _on_play_button_pressed() -> void:
 	$click_buttons.play()
 	await get_tree().create_timer(0.25).timeout
-	get_tree().change_scene_to_file("res://Map/map.tscn")
-
+	get_tree().change_scene_to_file("res://Intro/Intro.tscn")
 
 func _on_option_button_pressed() -> void:
 	$click_buttons.play()
